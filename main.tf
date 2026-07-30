9a1cc7dbd2c44a10805695f7983af926f9937362
Bhuvaneshwar Sannappareddy <bhuvaneshwars934@gmail.com>
2026-07-30 17:16:02 -0400

    Fix formatting of app_settings in main.tf

--- main.tf
+++ main.tf
@@ -47,8 +47,8 @@ resource "azurerm_linux_web_app" "agent" {
   service_plan_id     = azurerm_service_plan.app.id
 
   app_settings = {
-  SCM_DO_BUILD_DURING_DEPLOYMENT = "true"
-}
+    SCM_DO_BUILD_DURING_DEPLOYMENT = "true"
+  }
 
   site_config {
     app_command_line = "gunicorn --bind=0.0.0.0:8000 app:app"
